576e3aef7f5623337fcc15400426a6c9a43e7d87
Bill Leonard <stonybendbarn@gmail.com>
2026-01-02 13:31:06 -0500

    Add test card route

---
+++ app/cards/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestCardPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Rookie Run – Test Card</h1>
+      <p>If you can see this, QR routing works.</p>
+    </main>
+  );
+}
